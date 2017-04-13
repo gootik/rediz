08d932035f71e2c18c2220527435341d046824f2
Sasan Hezarkhani <gootik_2003@hotmail.com>
2017-04-13 16:31:15 +0100

    A few bug fixes and decoder tests (#17)
    * A few bug fixes and decoder tests
    
    * Array tests

---
+++ test/decoder_test.erl
@@ -0,0 +1,30 @@
+-module(decoder_test).
+
+-include_lib("eunit/include/eunit.hrl").
+-compile(export_all).
+
+
+decode_error_test() ->
+    {{error, <<"Some error message">>}, <<>>} = rediz_decoder:resp_decode(<<"-Some error message\r\n">>),
+    {{error, <<"-double dash">>}, <<>>} = rediz_decoder:resp_decode(<<"--double dash\r\n">>),
+    {{error, <<"-double dash">>}, <<"+ExtraStuff">>} = rediz_decoder:resp_decode(<<"--double dash\r\n+ExtraStuff">>).
+
+decode_integer_test() ->
+    {{ok, 100}, <<>>} = rediz_decoder:resp_decode(<<":100\r\n">>),
+    {{ok, -10000}, <<>>} = rediz_decoder:resp_decode(<<":-10000\r\n">>),
+    {{ok, 999}, <<"+ExtraStuff">>} = rediz_decoder:resp_decode(<<":999\r\n+ExtraStuff">>).
+
+decode_string_test() ->
+    {{ok, <<"100">>}, <<>>} = rediz_decoder:resp_decode(<<"+100\r\n">>),
+    {{ok, <<"+OK">>}, <<>>} = rediz_decoder:resp_decode(<<"++OK\r\n">>),
+    {{ok, <<"999">>}, <<"+ExtraStuff">>} = rediz_decoder:resp_decode(<<"+999\r\n+ExtraStuff">>).
+
+decode_bulk_string_test() ->
+    {{ok, <<"a">>}, <<>>} = rediz_decoder:resp_decode(<<"$1\r\na\r\n">>),
+    {buffer, <<"$10\r\n123456\r\n">>} = rediz_decoder:resp_decode(<<"$10\r\n123456\r\n">>),
+    {{ok, undefined}, <<"+ExtraStuff">>} = rediz_decoder:resp_decode(<<"$-1\r\n+ExtraStuff">>).
+
+decode_array_test() ->
+    {{ok, [2, <<"b">>, <<"a">>]}, <<>>} = rediz_decoder:resp_decode(<<"*3\r\n+a\r\n+b\r\n:2\r\n">>),
+    {buffer, <<"*5\r\n+a\r\n+b\r\n:2\r\n">>} = rediz_decoder:resp_decode(<<"*5\r\n+a\r\n+b\r\n:2\r\n">>),
+    {{ok, [2, <<"b">>, <<"a">>]}, <<"+ExtraStuff">>} = rediz_decoder:resp_decode(<<"*3\r\n+a\r\n+b\r\n:2\r\n+ExtraStuff">>).
